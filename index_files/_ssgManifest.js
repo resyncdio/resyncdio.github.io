03f663d134e7797c6034274c10d66f1a6e94d3ae
resyncdio <127646964+resyncdio@users.noreply.github.com>
2023-03-16 12:37:41 +0800

    Add files via upload

---
+++ index_files/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Ffractional-cto","\u002Foutsourcing","\u002Fremote","\u002Fcommunity","\u002Fjoin","\u002Fclients","\u002Fservices","\u002F"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
